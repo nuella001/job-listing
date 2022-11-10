ddeff0def7248f2f10b0dac80263bec132a9cc99
nuella001 <chiomamemmanuella@gmail.com>
2022-11-10 11:17:36 -0800

    added images

---
+++ src/assests/static-job-listings-master/group.js
@@ -0,0 +1,16 @@
+const images = [
+    { id: 1, src: './images/account.svg', title: 'foo', description: 'bar' },
+    { id: 2, src: './images/eyecam-co.svg', title: 'foo', description: 'bar' },
+    { id: 3, src: './images/faceit.svg', title: 'foo', description: 'bar' },
+    { id: 3, src: './images/favicon-32x32.png', title: 'foo', description: 'bar' },
+    { id: 4, src: './images/icon-remove.svg', title: 'foo', description: 'bar' },
+    { id: 5, src: './images/insure.svg', title: 'foo', description: 'bar' },
+    { id: 5, src: './images/loop-studios.svg', title: 'foo', description: 'bar' },
+    { id: 5, src: './images/manage.svg', title: 'foo', description: 'bar' },
+    { id: 5, src: './images/myhome.svg', title: 'foo', description: 'bar' },
+    { id: 5, src: './images/photosnap.svg', title: 'foo', description: 'bar' },
+    { id: 5, src: './images/shortly.svg', title: 'foo', description: 'bar' },
+    { id: 5, src: './images/the-air-filter-company.svg', title: 'foo', description: 'bar' },
+    
+  ];
+  export default images
--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// import photosnap from '../assests/static-job-listings-master/images/photosnap.svg'
 
 const Jobs = (props) => {
   if (props.job.id === 1 || props.job.id === 2) {
@@ -6,13 +7,13 @@ const Jobs = (props) => {
 
   return (
     <div className=" flex justify-center w-full">
-      <div className="w-11/12 mx-auto">
+      <div className="w-10/12 mx-auto">
         <div className="bg-white rounded relative w-full mt-12 p-4 flex-col space-y-[14px]">
           <div className="logo">
-            <img src="" alt="" className="absolute" />
+            <img src={props.job.logo} alt="" className="absolute" />
           </div>
           <div>
-            <h3>{props.job.company} </h3>
+            <h3 className='text-Dark-Cyan text-lg'>{props.job.company} </h3>
             <p>{props.job.new}</p>
             <p>{props.job.featured}</p>
           </div>
@@ -21,7 +22,11 @@ const Jobs = (props) => {
           </h1>
           <div className="spans flex  ">
             <span className="mr-4">{props.job.postedAt}</span>
+            <span className="w-1 h-1  rounded-full bg-Very-Dark-Grayish-Cyan text-center align-middle
+            justify-center mt-2 mr-2"></span>
             <span className="mr-4">{props.job.contract}</span>
+            <span className="w-1 h-1  rounded-full bg-Very-Dark-Grayish-Cyan text-center align-middle
+            justify-center mt-2 mr-2"></span>
             <span className="mr-4">{props.job.location}</span>
           </div>
           <div className="my-8 md:hidden">
